1f0f442613cb44e3bc07668129a702fadcbe24a7
Piper Gragg <pgragg@dwight.edu>
2015-06-30 15:22:02 -0400

    Another tweak for Heroku

--- db/migrate/20150630181729_remove_type_from_daily_lunches.rb
+++ db/migrate/20150630181729_remove_type_from_daily_lunches.rb
@@ -7,8 +7,6 @@ class RemoveTypeFromDailyLunches < ActiveRecord::Migration
     add_column :daily_lunches, :name, :string
     add_column :daily_lunches, :vegetarian, :boolean
     add_column :daily_lunches, :smart, :boolean
-    remove_column :lunches, :drink, :string 
-    remove_column :lunches, :bagel_filling, :string 
     add_column :daily_lunches, :lunch_type, :string
     add_column :daily_lunches, :date_array, :array
     add_column :lunches, :lunch_type, :string
