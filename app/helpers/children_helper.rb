852e21b440199bfe5c379918a878aa7bff2af0da
Piper Gragg <pgragg@dwight.edu>
2015-07-01 12:28:22 -0400

    Style drinks and lunches to be centered.

--- app/helpers/children_helper.rb
+++ app/helpers/children_helper.rb
@@ -2,7 +2,7 @@ module ChildrenHelper
 
   def button_class
     value = @user.children.count >= 1 ? "" : "btn-lg btn-midpage"
-    "btn btn-success #{value}"
+    "btn btn-success btn_bottom #{value}"
   end 
 
 end
