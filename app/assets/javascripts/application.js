baf95e90c03181e11e54836241fe9153f3678980
Piper Gragg <pgragg@dwight.edu>
2015-07-02 10:28:58 -0400

    cosmetic update to alerts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -24,3 +24,11 @@
 //         $(".lunch_description").show();
 //     });
 // });
+
+$(document).ready(function(){
+  setTimeout(function(){
+    $('.alert').fadeOut("slow", function() {
+      $(this).remove();
+    })
+  }, 4500);
+});
