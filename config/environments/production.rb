259740e2e179ab52d952f02805c999b67e84723d
Piper Gragg <pgragg@dwight.edu>
2015-07-03 14:40:56 -0400

    changed host to herokuapp.com in production.

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -13,7 +13,8 @@ Rails.application.configure do
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = false
   config.action_controller.perform_caching = true
-
+  config.action_mailer.default_url_options = { :host => "http://dwight-lunchbox.herokuapp.com" }
+  Rails.application.routes.default_url_options[:host] = "http://dwight-lunchbox.herokuapp.com" 
   # Enable Rack::Cache to put a simple HTTP cache in front of your application
   # Add `rack-cache` to your Gemfile before enabling this.
   # For large-scale production use, consider using a caching reverse proxy like
